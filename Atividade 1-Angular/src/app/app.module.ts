59390df6d9b46b87ac111f01855be53e54d34aba
Afranio <afraniodimadasilva@gmail.com>
2022-04-11 21:56:36 -0300

    angular

---
+++ Atividade 1-Angular/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContatoComponent } from './views/contato/contato.component';
+import { JogadorComponent } from './views/jogador/jogador.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { HttpClientModule} from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContatoComponent,
+    JogadorComponent,
+    HomeComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
